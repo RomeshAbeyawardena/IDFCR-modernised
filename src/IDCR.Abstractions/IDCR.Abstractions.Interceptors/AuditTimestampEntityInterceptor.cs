53feed1e9b719622cbd9eb9b3ec085ba37b3ca41
Romesh Abeyawardena <romesh.a@outlook.com>
2026-03-20 20:09:49 +0000

    wip

--- src/IDCR.Abstractions/IDCR.Abstractions.Interceptors/AuditTimestampEntityInterceptor.cs
+++ src/IDCR.Abstractions/IDCR.Abstractions.Interceptors/AuditTimestampEntityInterceptor.cs
@@ -1,22 +1,57 @@
 ﻿namespace IDCR.Abstractions.Interceptors;
 
-public class AuditTimestampEntityInterceptor(TimeProvider timeProvider) : IEntityInterceptor
+public abstract class EntityInterceptorBase(EntityContextBehaviorStage stage, EntityContextBehavior behavior, int? orderIndex = null) : IEntityInterceptor
 {
-    public int? OrderIndex { get; } = 0;
+    public int? OrderIndex { get; } = orderIndex;
 
-    public bool CanIntercept(IEntityInterceptorContext context)
+    public virtual bool ShouldIntercept(IEntityInterceptorContext context) => true;
+
+    public virtual bool CanIntercept(IEntityInterceptorContext context)
     {
-        return context.Stage == EntityContextBehaviorStage.Pre
-            && context.Behavior == EntityContextBehavior.Insert
-            && context.Model is IAuditCreatedTimestamp auditCreatedTimestamp && auditCreatedTimestamp.CreatedTimestampUtc == default;
+        return context.Stage == stage
+            && context.Behavior == behavior
+            && ShouldIntercept(context);
+    }
+    public abstract void Intercept(IEntityInterceptorContext context);
+}
+
+public sealed class AuditCreatedTimestampEntityInterceptor(TimeProvider timeProvider) 
+    : EntityInterceptorBase(EntityContextBehaviorStage.Pre, EntityContextBehavior.Insert, 0)
+{
+    public override  bool ShouldIntercept(IEntityInterceptorContext context)
+    {
+        return context.Model is IAuditCreatedTimestamp auditCreatedTimestamp 
+            && auditCreatedTimestamp.CreatedTimestampUtc == default;
     }
 
-    public void Intercept(IEntityInterceptorContext context)
+    public override  void Intercept(IEntityInterceptorContext context)
     {
-        //we don't know when this will be called and if the state of the object changed so we might as well grab the fresh state
         if (context.Model is IAuditCreatedTimestamp auditCreatedTimestamp)
         {
             auditCreatedTimestamp.CreatedTimestampUtc = timeProvider.GetUtcNow();
+        }
+    }
+}
+
+public interface IAuditModifiedTimestamp
+{
+    DateTimeOffset? ModifiedTimestampUtc { get; set; }
+}
+
+public sealed class AuditModifiedTimestampEntityInterceptor(TimeProvider timeProvider) 
+    : EntityInterceptorBase(EntityContextBehaviorStage.Pre, EntityContextBehavior.Update, 0)
+{
+    public override bool ShouldIntercept(IEntityInterceptorContext context)
+    {
+        return context.Model is IAuditModifiedTimestamp auditCreatedTimestamp
+            && auditCreatedTimestamp.ModifiedTimestampUtc == default;
+    }
+
+    public override void Intercept(IEntityInterceptorContext context)
+    {
+        if (context.Model is IAuditModifiedTimestamp auditModifiedTimestamp)
+        {
+            auditModifiedTimestamp.ModifiedTimestampUtc = timeProvider.GetUtcNow();
         }
     }
 }
