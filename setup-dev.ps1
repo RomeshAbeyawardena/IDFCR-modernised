09fe96e021406bfae5721d6acb8c66f4efc038e6
Romesh Abeyawardena <Romesh.Abeyawardena@bellrock.co.uk>
2026-03-26 15:53:22 +0000

    Updated scripts

--- setup-dev.ps1
+++ setup-dev.ps1
@@ -1,9 +1,9 @@
 param (
-    [securestring] $password
+    [string]$secret
 )
 
-$env:DEV_CONN_STRING = "Server=localhost,5050;Initial Catalog=master;TrustServerCertificate=true;Connection Timeout=1000;User Id=sa;Password=$password"
+$env:DEV_CONN_STRING = "Server=localhost,5050;Initial Catalog=master;TrustServerCertificate=true;Connection Timeout=1000;User Id=sa;Password=$secret"
 [System.Environment]::SetEnvironmentVariable("DevConnString", $env:DEV_CONN_STRING, "User")
 
 $currentDirectory = Get-Location;
-[System.IO.File]::WriteAllText([System.IO.Path]::Combine($currentDirectory, ".env"), "SA_PASSWORD=$password");
+[System.IO.File]::WriteAllText([System.IO.Path]::Combine($currentDirectory, ".env"), "SA_PASSWORD=$secret");
