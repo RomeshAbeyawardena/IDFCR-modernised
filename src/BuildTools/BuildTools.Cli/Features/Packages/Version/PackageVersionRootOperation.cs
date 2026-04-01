728963a156e688726eebd82b7b81e3964b9fde5b
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-01 19:51:22 +0100

    wip

--- src/BuildTools/BuildTools.Cli/Features/Packages/Version/PackageVersionRootOperation.cs
+++ src/BuildTools/BuildTools.Cli/Features/Packages/Version/PackageVersionRootOperation.cs
@@ -1,12 +1,27 @@
+using BuildTools.Cli.Extensions;
+using BuildTools.Cli.ManagedStreams;
 using BuildTools.Cli.Operations;
 
 namespace BuildTools.Cli.Features.Packages.Version;
 
-[FeatureCommand(PackageVersionRootOperation.Prefix, CommandName)]
+[FeatureCommand(Prefix, CommandName)]
 public class PackageVersionRootOperation(IServiceProvider serviceProvider)
     : InjectableCommandOperationRootBase<PackageVersionRootOperation>(serviceProvider, Prefix,
         CommandName, typeof(PackageVersionRootOperation))
 {
-    public const string Prefix = $"{Packages.PackageRootOperation.Prefix}-version";
+    public const string Prefix = $"{PackageRootOperation.Prefix}-version-";
+    public const string CommandName = "version";
+}
+
+[FeatureCommand(PackageVersionRootOperation.Prefix, CommandName)]
+public class PackageVersionReadOperation(IServiceProvider serviceProvider, IManagedStream managedStream) 
+    : InjectableCommandOperationBase<PackageVersionReadOperation>(serviceProvider, PackageVersionRootOperation.Prefix,
+        CommandName, typeof(PackageVersionRootOperation))
+{
     public const string CommandName = "read";
+    protected override async Task InvokeWhenContextIsOwned(IEnumerable<string> command, CancellationToken cancellationToken)
+    {
+        var newVersion = await this.GetOptionalField(managedStream, command, cancellationToken, false, "new-version");
+        
+    }
 }
