fbd54a4e0e394ffc47335e61f2514a5f3ff04b22
Romesh Abeyawardena <Romesh.Abeyawardena@bellrock.co.uk>
2026-04-02 10:36:44 +0100

    wip

--- src/IDFCR.Abstractions/IDFCR.Abstractions.Results/Exceptions/IExposableException.cs
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.Results/Exceptions/IExposableException.cs
@@ -1,10 +1,19 @@
 ﻿namespace IDFCR.Abstractions.Results.Exceptions;
 
+
 /// <summary>
-/// Interface for exceptions that can be exposed to the client.
+/// Defines an interface for exceptions that can be safely exposed to external clients.
+/// Implementing this interface indicates that the exception contains information suitable for client consumption.
 /// </summary>
 public interface IExposableException
 {
+    /// <summary>
+    /// Gets the message that describes the exception.
+    /// </summary>
     string Message { get; }
+    
+    /// <summary>
+    /// Gets additional details about the exception, if available.
+    /// </summary>
     string? Details { get; }
 }
