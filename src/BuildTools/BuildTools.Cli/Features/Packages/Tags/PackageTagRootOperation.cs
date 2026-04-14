eb2ccb85704338b0e5f5b7e8fe8f18aef6a5afe0
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-14 21:47:25 +0100

    Add PackageTagRootOperation for package tag subcommand
    Introduced the PackageTagRootOperation class in the BuildTools.Cli.Features.Packages.Tags namespace. This class enables a new "tag" subcommand under the package root operation, supports dependency injection, and is registered with the FeatureCommand attribute for command routing.

---
+++ src/BuildTools/BuildTools.Cli/Features/Packages/Tags/PackageTagRootOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IDFCR.Abstractions.Cli.Operations;
+
+namespace BuildTools.Cli.Features.Packages.Tags;
+
+[FeatureCommand(PackageRootOperation.Prefix, CommandName)]
+public class PackageTagRootOperation(IServiceProvider serviceProvider)
+    : InjectableCommandOperationBase<PackageTagRootOperation>(serviceProvider, PackageRootOperation.Prefix,
+        CommandName, typeof(PackageRootOperation))
+{
+    public const string Prefix = $"{PackageRootOperation.Prefix}-tag";
+    public const string CommandName = "tag";
+}
