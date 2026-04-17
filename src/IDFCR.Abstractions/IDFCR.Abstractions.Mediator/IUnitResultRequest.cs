4dd380709e0339226ba7f3a928917d5b6567c3a2
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-17 19:58:09 +0100

    Add Mediator abstractions and remove CLI suggestion logic
    Added IDFCR.Abstractions.Mediator and Extensions projects with MediatR-based request interfaces and base record for paged results. Removed SuggestionDiscovererRootCommand CLI logic. Updated solution to include new projects.

---
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.Mediator/IUnitResultRequest.cs
@@ -0,0 +1,20 @@
+﻿using IDFCR.Abstractions.Results;
+using MediatR;
+
+namespace IDFCR.Abstractions.Mediator;
+/// <summary>
+/// Represents a request that returns a unit result, which indicates the success or failure of an operation without returning any data. 
+/// </summary>
+public interface IUnitResultRequest : IRequest<IUnitResult>
+{
+
+}
+
+/// <summary>
+/// Represents a request that returns a unit result with a value of type T, which indicates the success or failure of an operation along with the associated data.
+/// </summary>
+/// <typeparam name="T">The type of result returned when successful</typeparam>
+public interface IUnitResultRequest<T> : IRequest<IUnitResult<T>>
+{
+
+}
---
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.Mediator/PagedUnitResultRequestBase.cs
@@ -0,0 +1,21 @@
+﻿using IDFCR.Abstractions.Results;
+using MediatR;
+
+namespace IDFCR.Abstractions.Mediator;
+
+/// <summary>
+/// Represents a base record for a request that returns a paged collection of unit results with values of type T. It provides a convenient way to create specific requests by inheriting from this base record and passing the necessary paging information through the constructor.
+/// </summary>
+/// <typeparam name="T">The type of result returned when successful</typeparam>
+public abstract record PagedUnitResultRequestBase<T>() : PagedQuery, IRequest<IUnitPagedResult<T>>
+{
+    /// <summary>
+    /// Initialises a new instance of the <see cref="PagedUnitResultRequestBase{T}"/> record with the specified paging information from an <see cref="IPagedQuery"/>. This constructor allows you to easily create a paged unit result request by passing an existing paged query, which can be useful for scenarios where you want to reuse paging parameters across different requests.
+    /// </summary>
+    /// <param name="pagedQuery">The paged query to copy paged parameters</param>
+    protected PagedUnitResultRequestBase(IPagedQuery pagedQuery) : this()
+    {
+        base.PageIndex = pagedQuery.PageIndex;
+        base.PageSize = pagedQuery.PageSize;
+    }
+}
