438c23d38114510b163fce8297f0836ea44ca6be
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-19 00:02:45 +0100

    Cache status report and improve root endpoint output
    Refactored the "/" endpoint to cache the status report, reducing redundant string building and improving performance. Introduced a thread-safe cachedReportData variable and SemaphoreSlim. Made the refresh interval configurable via a constant. Enhanced the status report with runtime start date, uptime, last update, and next update timestamps. Service list is now shown only in DEBUG builds. Improved formatting and ensured thread-safe access to cached data.

--- src/BuildTools/Backend/BuildTools.gRPC.Api/Program.cs
+++ src/BuildTools/Backend/BuildTools.gRPC.Api/Program.cs
@@ -31,29 +31,47 @@ foreach (var type in GrpcServices.Fetch())
 }
 
 var startTime = app.Services.GetRequiredService<TimeProvider>().GetUtcNow();
+string? cachedReportData = null;
 
+using SemaphoreSlim semaphoreSlim = new(0, 1);
+
+const int UpdateIntervalInSeconds = 10;
 app.MapGet("/", async(ctx) => {
-    ctx.Response.Headers.Append("Refresh", "10");
-    var newLine = Environment.NewLine;
-    var statusReport = new StringBuilder($"Build Tools gRPC Server{newLine}{newLine}Active Services:");
-
-    statusReport.AppendLine();
-    foreach (var service in serviceList)
+    ctx.Response.Headers.Append("Refresh", $"{UpdateIntervalInSeconds}");
+    StringBuilder statusReport = new();
+    if (string.IsNullOrEmpty(cachedReportData))
     {
-        statusReport.AppendLine($"\t- {service}");
-    }
+        var newLine = Environment.NewLine;
+        statusReport.AppendLine($"Build Tools gRPC Server{newLine}{newLine}");
 
-    statusReport.AppendLine();
+#if DEBUG
+        statusReport.AppendLine("Active Services:");
+
+        foreach (var service in serviceList)
+        {
+            statusReport.AppendLine($"\t- {service}");
+        }
+#endif
+
+        statusReport.AppendLine();
+
+        cachedReportData = statusReport.ToString();
+    }
+    else
+    {
+        await semaphoreSlim.WaitAsync();
+        statusReport.Append(cachedReportData);
+        semaphoreSlim.Release();
+    }
 
     var timeProvider = ctx.RequestServices.GetRequiredService<TimeProvider>();
     var timeNow = timeProvider.GetUtcNow();
     var uptime = timeNow.Subtract(startTime);
-
-    statusReport.AppendLine($"Server Uptime: {uptime:hh\\:mm\\:ss}");
-    statusReport.AppendLine($"Last updated: {timeNow: dd/MM/yyy HH:mm:ss}");
+    statusReport.AppendLine($"Runtime start date: {startTime.ToLocalTime(): dd/MM/yyyy HH:mm:ss}");
+    statusReport.AppendLine($"Server uptime: {uptime:hh\\:mm\\:ss}");
+    statusReport.AppendLine($"Page last updated: {timeNow.ToLocalTime(): dd/MM/yyyy HH:mm:ss}");
+    statusReport.AppendLine($"Next update at: {timeNow.AddSeconds(UpdateIntervalInSeconds).ToLocalTime(): dd/MM/yyyy HH:mm:ss}");
     await ctx.Response.WriteAsync(statusReport.ToString());
-
-    ///return $"Build tools GRPC Server\r\nActive Services:\r\n\t- {string.Join("\r\n\t- ", serviceList)}\r\n";
 });
 
 app.Run();
