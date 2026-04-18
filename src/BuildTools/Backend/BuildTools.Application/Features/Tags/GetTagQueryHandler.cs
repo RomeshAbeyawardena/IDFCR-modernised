86c568227efd64df987b2e8c54804633e98b9dde
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-18 13:50:17 +0100

    Update IDFCR.* packages to 2.0.3.3 and add query handler
    Updated multiple projects to use IDFCR.* version 2.0.3.3.
    Added `GetTagsQueryHandler` with stubbed implementation.
    Removed an unused using directive from `GetTagQueryHandler.cs`.

--- src/BuildTools/Backend/BuildTools.Application/Features/Tags/GetTagQueryHandler.cs
+++ src/BuildTools/Backend/BuildTools.Application/Features/Tags/GetTagQueryHandler.cs
@@ -1,18 +1,24 @@
-﻿using BuildTools.Shared.Contracts.Feature.Tags;
-using IDFCR.Abstractions.Mediator;
+﻿using BuildTools.Infrastructure.Features.Tags;
+using BuildTools.Shared.Contracts.Feature.Tags;
 using IDFCR.Abstractions.Mediator.Extensions;
 using IDFCR.Abstractions.Results;
+using IDFCR.Abstractions.Results.Extensions;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace BuildTools.Application.Features.Tags;
 
-public class GetTagQueryHandler : IUnitResultRequestHandler<GetTagQuery, TagDto>
+public class GetTagQueryHandler(ITagRepository tagRepository) : IUnitResultRequestHandler<GetTagQuery, TagDto>
 {
-    public Task<IUnitResult<TagDto>> Handle(GetTagQuery request, CancellationToken cancellationToken)
+    public async Task<IUnitResult<TagDto>> Handle(GetTagQuery request, CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        var tagResult = await tagRepository.GetTagAsync(request.Name, cancellationToken);
+
+        if (!tagResult.HasValue)
+        {
+            return UnitResult.Failed<TagDto>(tagResult.Exception, tagResult.Action, tagResult.FailureReason);
+        }
+
+        return UnitResult.FromResult(tagResult.GetResultOrDefault().Map<TagDto>(), tagResult.Action, true, tagResult.Exception);
     }
 }
-
-public class Get
---
+++ src/BuildTools/Backend/BuildTools.Application/Features/Tags/GetTagsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildTools.Infrastructure.Features.Tags;
+using BuildTools.Shared.Contracts.Feature.Tags;
+using IDFCR.Abstractions.Mediator.Extensions;
+using IDFCR.Abstractions.Results;
+using IDFCR.Abstractions.Results.Extensions;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BuildTools.Application.Features.Tags;
+
+public class GetTagsQueryHandler(ITagRepository tagRepository) : IUnitResultCollectionRequestHandler<GetTagsQuery, TagDto>
+{
+    public async Task<IUnitResultCollection<TagDto>> Handle(GetTagsQuery request, CancellationToken cancellationToken)
+    {
+        var tagsResult = await tagRepository.GetExistingTagsAsync(request.Names, cancellationToken);
+
+        if (!tagsResult.HasValue)
+        {
+            return UnitResultCollection.Failed<TagDto>(tagsResult.Exception, tagsResult.Action);
+        }
+
+        return UnitResultCollection.FromResult(tagsResult.GetResultOrDefault().Select(x => x.Map<TagDto>()), tagsResult.Action, true);
+    }
+}
