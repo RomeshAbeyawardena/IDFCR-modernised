bbb6a78b8b952cc0828cff96c566bb68d5c2355c
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-19 15:50:16 +0100

    Add proto packaging, cache flush, and unit result proto
    Updated csproj to package proto files and enable codegen. Added FlushCache to RegisteredGRPCServices and fixed parameter typo. Introduced unit_result.proto for standardized gRPC responses.

--- src/IDFCR.Abstractions/IDFCR.Abstractions.gRPC/RegisteredGRPCServices.cs
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.gRPC/RegisteredGRPCServices.cs
@@ -11,6 +11,9 @@ public static class RegisteredGRPCServices
     private static readonly Lazy<IRegisteredGRPCServiceImplementationTypeDiscoveryService> registeredGRPCServiceImplementationTypeDiscoveryService = 
         new(new RegisteredGRPCServiceImplementationTypeDiscoveryService());
 
+    /// <summary>
+    /// Flushes the cache of discovered types in the RegisteredGRPCServiceImplementationTypeDiscoveryService. This method can be called to clear the cache of discovered types, which will force a fresh discovery of types the next time the DiscoverTypes method is called. This can be useful if you want to ensure that any changes to the assemblies being searched or the configuration are reflected in the results of the DiscoverTypes method, without needing to restart the application or create a new instance of the RegisteredGRPCServiceImplementationTypeDiscoveryService class.
+    /// </summary>
     public static void FlushCache()
     {
         registeredGRPCServiceImplementationTypeDiscoveryService.Value.FlushCache();
@@ -37,12 +40,12 @@ public static class RegisteredGRPCServices
     /// Discovers types that are decorated with the RegisteredGRPCServiceImplementationAttribute in the specified assemblies. The method returns an enumerable of types that are decorated with the RegisteredGRPCServiceImplementationAttribute and meet the criteria specified by the attribute's parameters. The configuration parameter can be used to evaluate the EnabledValueConfigurationKey for each type if necessary. This method uses a lazy singleton instance of the RegisteredGRPCServiceImplementationTypeDiscoveryService to perform the discovery of types, so it will cache the results of the discovery and return the cached results on subsequent calls with the same assemblies, unless the FlushCache method is called to clear the cache. This allows for efficient discovery of types without redundant reflection operations on subsequent calls with the same assemblies. If you need to force a fresh discovery of types, you can call the DiscoverTypes method with flashCache set to true, which will flush the cache before performing the discovery.
     /// </summary>
     /// <param name="configuration">The application's configuration, which can be used to evaluate the EnabledValueConfigurationKey for each type if necessary.</param>
-    /// <param name="flashCache">A boolean value indicating whether to flush the cache of discovered types before performing the discovery. If true, the cache will be flushed and a fresh discovery of types will be performed. If false, the cached results will be returned if the same assemblies are searched again.</param>
+    /// <param name="flushCache">A boolean value indicating whether to flush the cache of discovered types before performing the discovery. If true, the cache will be flushed and a fresh discovery of types will be performed. If false, the cached results will be returned if the same assemblies are searched again.</param>
     /// <param name="assemblies">The assemblies to search for types.</param>
     /// <returns>An enumerable of types that are decorated with the RegisteredGRPCServiceImplementationAttribute and meet the criteria specified by the attribute's parameters.</returns>
-    public static IEnumerable<Type> DiscoverTypes(IConfiguration configuration, bool flashCache, params Assembly[] assemblies)
+    public static IEnumerable<Type> DiscoverTypes(IConfiguration configuration, bool flushCache, params Assembly[] assemblies)
     {
-        if(flashCache)
+        if(flushCache)
         {
             registeredGRPCServiceImplementationTypeDiscoveryService.Value.FlushCache();
         }
