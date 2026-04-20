bf36b3c0ee15049ea23e0b7c8f0b036c6bfe31f1
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-20 21:35:19 +0100

    wip

---
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.gRPC.Generated/Test.cs
@@ -0,0 +1,10 @@
+﻿namespace IDFCR.Abstractions.GRPC.Generated
+{
+    /// <summary>
+    /// Represents a marker used to indicate a specific point or concept within the application.
+    /// </summary>
+    public sealed class Marker
+    {
+
+    }
+}
