1737ef939699e49763cf3c49ab9f865aa78d7aa3
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-22 23:01:14 +0100

    wip

---
+++ src/BuildTools/Backend/BuildTools.Shared.Contracts/Features/SystemSettings/GetPagedSystemSettingsQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildTools.Shared.Contracts.Features.SystemSettings
+{
+    internal class GetPagedSystemSettingsQuery
+    {
+    }
+}
