a7e36fc507b8a2a46d3d9c98dcec6ff12aa95bb2
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-25 11:36:14 +0100

    wip

---
+++ src/BuildTools/BuildTools.Infrastructure.SqlServer/Features/Settings/Processors/SettingAuditProcessor.cs
@@ -0,0 +1,13 @@
+﻿using BuildTools.Shared.Features.Settings;
+using IDFCR.Abstractions.Interceptors;
+using IDFCR.Abstractions.Results;
+
+namespace BuildTools.Infrastructure.SqlServer.Features.Settings.Processors;
+
+public class SettingAuditProcessor() : AuditProcessorBase<SettingEntity, SettingAuditEntity>(nameof(Setting))
+{
+    public override Task<IUnitResult> AuditChangesAsync(SettingEntity oldValue, SettingEntity newValue, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.Interceptors.Extensions/AuditProcessorExtensions.cs
@@ -0,0 +1,42 @@
+﻿using FastMember;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace IDFCR.Abstractions.Interceptors.Extensions;
+
+public static class AuditProcessorExtensions
+{
+    private static readonly ConcurrentDictionary<Type, string[]> _mappableMemberCache = [];
+#pragma warning disable IDE0060
+    public static string ChangeDescriptor<TEntity>(
+        this IAuditProcessor auditProcessor, TEntity oldEntity, TEntity newEntity)
+    {
+        var changeDescriptions = new StringBuilder();
+
+        if (oldEntity is null || newEntity is null)
+        {
+            return changeDescriptions.ToString();
+        }
+
+        var type = oldEntity.GetType();
+        var sourceAccessor = TypeAccessor.Create(type);
+
+        var sourceTypeNames = _mappableMemberCache.GetOrAdd(type, (t) => [.. sourceAccessor.GetMembers().Where(m => m.CanRead).Select(m => m.Name)]);
+
+        foreach(var name in sourceTypeNames)
+        {
+            var oldValue = sourceAccessor[oldEntity, name];
+            var newValue = sourceAccessor[newEntity, name];
+
+            if(object.Equals(oldValue, newValue))
+            {
+                continue;
+            }
+
+            changeDescriptions.AppendLine($"{name} changed from '{oldValue}' to {newValue}.");
+        }
+
+        return changeDescriptions.ToString();
+    }
+#pragma warning restore
+}
