29b5c65036065cc328ecfb3370fd79ee13f78284
Romesh Abeyawardena <romesh.a@outlook.com>
2026-04-26 15:26:06 +0100

    wp

--- src/BuildTools/BuildTools.Infrastructure.SqlServer/Features/Settings/Processors/SettingAuditProcessor.cs
+++ src/BuildTools/BuildTools.Infrastructure.SqlServer/Features/Settings/Processors/SettingAuditProcessor.cs
@@ -1,34 +1,35 @@
-﻿using BuildTools.Infrastructure.Features.Environments;
-using IDFCR.Abstractions.Interceptors.Processors;
+﻿using IDFCR.Abstractions.Interceptors.Processors;
 using IDFCR.Abstractions.Interceptors.Extensions;
 using IDFCR.Abstractions.Results;
-using IDFCR.Abstractions.Results.Extensions;
-using Microsoft.Extensions.DependencyInjection;
+
 using System.Text.Json;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace BuildTools.Infrastructure.SqlServer.Features.Settings.Processors;
 
 public class SettingAuditProcessor() : AuditProcessorBase<SettingEntity, SettingAuditEntity>(nameof(SettingEntity))
 {
+    private PackageManagerDbContext? context;
+
     private async Task<object?> LookupAsync(string key, object value, CancellationToken cancellationToken)
     {
-        //var environmentRepository = serviceProvider.GetRequiredService<IEnvironmentRepository>();
-        //if (key == "Environment" && value is Guid id)
-        //{
-        //    var environment = (await environmentRepository.FindAsync(id, cancellationToken)).GetResultOrDefault();
+        if (key == "Environment" && value is Guid id)
+        {
+            var environment = (await context!.Environments.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id, cancellationToken));
 
-        //    if (environment is not null)
-        //    {
-        //        return environment.DisplayName ?? environment.Name;
-        //    }
-        //}
+            if (environment is not null)
+            {
+                return environment.DisplayName ?? environment.Name;
+            }
+        }
 
         return null;
     }
 
     public override async Task<IUnitResult> AuditChangesAsync(SettingEntity oldValue, SettingEntity newValue, CancellationToken cancellationToken)
     {
-        if (Provider!.InterceptorFactory!.ScopedResources.TryGetScopedResource<PackageManagerDbContext>(out var context))
+        if (Provider!.InterceptorFactory!.ScopedResources.TryGetScopedResource<PackageManagerDbContext>(out context))
         {
             var changes = await this.AuditChangeDescriptionAsync(oldValue, newValue, cancellationToken, deferredLookupAsyncAction: LookupAsync);
 
@@ -40,16 +41,7 @@ public class SettingAuditProcessor() : AuditProcessorBase<SettingEntity, Setting
                 ChangeDescription = changes
             });
         }
-        //var context = scope.ServiceProvider.GetRequiredService<PackageManagerDbContext>();
-        //
-        //context.SettingAudits.Add(new SettingAuditEntity
-        //{
-        //    OldValueJson = JsonSerializer.Serialize(oldValue),
-        //    NewValueJson = JsonSerializer.Serialize(newValue),
-        //    SettingId = newValue.Id,
-        //    ChangeDescription = changes
-        //});
-
+        
         return UnitResult.Success(UnitAction.Add);
     }
 }
