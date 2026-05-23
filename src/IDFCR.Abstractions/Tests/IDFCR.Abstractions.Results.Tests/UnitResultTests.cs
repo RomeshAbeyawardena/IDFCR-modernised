ab422eb43930cdaf1a1762fdc76194ce4abda34e
Romesh Abeyawardena <romesh.a@outlook.com>
2026-05-23 08:44:39 +0100

    Add UnitResultTests for success and state modification
    Introduced UnitResultTests using NUnit to verify UnitResult.Success behavior and test state modification logic for both generic and non-generic UnitResult instances.

---
+++ src/IDFCR.Abstractions/Tests/IDFCR.Abstractions.Results.Tests/UnitResultTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace IDFCR.Abstractions.Results.Tests;
+
+[TestFixture]
+internal class UnitResultTests
+{
+    [Test]
+    public void Simple_SuccessPath()
+    {
+        var result = UnitResult.Success(UnitAction.Add);
+        Assert.AreEqual(UnitAction.Add, result.Action);
+        Assert.IsTrue(result.IsSuccess);
+    }
+
+    [Test]
+    public void ModifiedState_Path()
+    {
+        var result = UnitResult.Success(UnitAction.Get);
+        Assert.That(result.IsSuccess, Is.True);
+        //can't set a non-generic UnitResult's modified state because it has no actual state it is storing
+        Assert.That(result.TrySetState(Guid.NewGuid()), Is.False);
+
+        var id = Guid.NewGuid();
+        var idResult = UnitResult.FromResult(id);
+        //can set a generic unit result's modified state
+        Assert.That(idResult.IsSuccess, Is.True);
+        Assert.That(idResult.TrySetState(Guid.NewGuid()), Is.True);
+        Assert.That(idResult.Result, Is.Not.EqualTo(id));
+        Assert.That(idResult.OriginalState, Is.EqualTo(id));
+    }
+}
