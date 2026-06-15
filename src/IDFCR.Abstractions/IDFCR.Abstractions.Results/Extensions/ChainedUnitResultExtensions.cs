ac6b57c0a3d73bf51d038a770aaf94353873f623
Romesh Abeyawardena <romesh.a@outlook.com>
2026-06-15 18:18:42 +0100

    Refactor: improve readability of return statements
    Reformatted return statements in ChainedUnitResultExtensions by moving the null-coalescing operator (??) to a new line for better readability. No changes to logic or functionality.

--- src/IDFCR.Abstractions/IDFCR.Abstractions.Results/Extensions/ChainedUnitResultExtensions.cs
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.Results/Extensions/ChainedUnitResultExtensions.cs
@@ -22,7 +22,8 @@ public static class ChainedUnitResultExtensions
 
         if (!enumerator.MoveNext())
         {
-            return result as IChainedUnitResult<T> ?? new DefaultChainedUnitResult<T>(result, UnitResult.Create(true), setAsFailWhenAnyUnitsFail);
+            return result as IChainedUnitResult<T> 
+                ?? new DefaultChainedUnitResult<T>(result, UnitResult.Create(true), setAsFailWhenAnyUnitsFail);
         }
 
         IChainedUnitResult<T> currentChain = new DefaultChainedUnitResult<T>(result, enumerator.Current);
@@ -58,7 +59,8 @@ public static class ChainedUnitResultExtensions
 
         if (!enumerator.MoveNext())
         {
-            return result as IChainedUnitResult ?? new DefaultChainedUnitResult(result, UnitResult.Create(true), setAsFailWhenAnyUnitsFail);
+            return result as IChainedUnitResult 
+                ?? new DefaultChainedUnitResult(result, UnitResult.Create(true), setAsFailWhenAnyUnitsFail);
         }
 
         IChainedUnitResult currentChain = new DefaultChainedUnitResult(result, enumerator.Current);
