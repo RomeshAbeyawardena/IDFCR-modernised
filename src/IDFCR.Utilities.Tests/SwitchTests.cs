988cbaf5fead835ec8a1a60fb580326d2a842ec5
Romesh Abeyawardena <romesh.a@outlook.com>
2026-06-27 18:01:32 +0100

    Add IDFCR.Utilities.Tests project with NUnit setup
    Added a new test project, IDFCR.Utilities.Tests, targeting .NET 10.0. Included necessary test dependencies such as coverlet.collector, Microsoft.NET.Test.Sdk, NUnit, NUnit.Analyzers, and NUnit3TestAdapter. Updated the solution file to include the new test project under the /src/IDFCR.Utilities/ folder.

---
+++ src/IDFCR.Utilities.Tests/SwitchTests.cs
@@ -0,0 +1,41 @@
+﻿
+using NUnit.Framework;
+
+namespace IDFCR.Utilities.Tests;
+
+[TestFixture]
+internal class SwitchTests
+{
+    private static readonly string[] Values = ["foo", "bar", "roo", "moo", "two"];
+    private static string GetValue(int index)
+    {
+        return Values[index];
+    }
+    private const string InvalidOptionMessage = "Invalid option.";
+    private ISwitch<int, string> sut;
+    [SetUp]
+    public void SetUp()
+    {
+        sut = SwitchBuilder.Build<int, string>(config =>
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                config.CaseWhen(i, GetValue);
+            }
+
+            config.Else((key) => InvalidOptionMessage);
+        });
+    }
+
+    [Test]
+    public void Test()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var m = Values[0];
+            Assert.That(sut.ThenValue(0), Is.EqualTo(m));
+        }
+
+        Assert.That(sut.ThenValue(-1), Is.EqualTo(InvalidOptionMessage));
+    }
+}
