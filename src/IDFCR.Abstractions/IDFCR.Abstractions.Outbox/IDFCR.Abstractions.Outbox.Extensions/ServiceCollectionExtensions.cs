5a6597fa615a4c0d5859b65ab3cf8c441f0df29b
Romesh Abeyawardena <romesh.a@outlook.com>
2026-07-05 18:27:25 +0100

    Make AddOutboxPattern generic for handler injection
    Refactored AddOutboxPattern to accept a generic TOutboxEntityNotificationHandler, enabling consumer-defined handler registration. Updated XML docs to describe the new generic parameter and registration behavior.

--- src/IDFCR.Abstractions/IDFCR.Abstractions.Outbox/IDFCR.Abstractions.Outbox.Extensions/ServiceCollectionExtensions.cs
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.Outbox/IDFCR.Abstractions.Outbox.Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿
 using IDFCR.Abstractions.DependencyInjection.Extensions;
 using IDFCR.Abstractions.Interceptors.Interceptors;
+using IDFCR.Abstractions.Outbox.Handlers;
 using IDFCR.Abstractions.Outbox.Interceptors;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,13 +15,16 @@ public static class ServiceCollectionExtensions
     /// <summary>
     /// Adds services related to the outbox pattern to the specified <see cref="IServiceCollection"/>. This method registers the necessary services for processing outbox messages and tracking their status, allowing developers to easily integrate outbox functionality into their applications and systems that utilize an outbox pattern for reliable message delivery and tracking of message status.
     /// </summary>
+    /// <typeparam name="TOutboxEntityNotificationHandler">The type of the outbox entity notification handler to be registered.</typeparam>
     /// <param name="services">The <see cref="IServiceCollection"/> to which the outbox services will be added.</param>
     /// <returns>The updated <see cref="IServiceCollection"/> with the outbox services added.</returns>
-    public static IServiceCollection AddOutboxPattern(IServiceCollection services)
+    public static IServiceCollection AddOutboxPattern<TOutboxEntityNotificationHandler>(IServiceCollection services)
+        where TOutboxEntityNotificationHandler : class, IOutboxEntityNotificationHandler
     {
         var assembly = typeof(OutboxInterceptor).Assembly;
 
         return services
+            .AddTransient<IOutboxEntityNotificationHandler, TOutboxEntityNotificationHandler>()
             .ScanGenericServices<IEntityInterceptor>(ServiceLifetime.Transient, assembly);
     }
 }
