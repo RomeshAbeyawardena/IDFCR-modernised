e64c51edfc380231e46b4271ae82fde9a57029ff
Romesh Abeyawardena <romesh.a@outlook.com>
2026-07-05 17:15:51 +0100

    Update tests to reflect removal of OutboxInterceptor
    Adjusted ServiceCollectionExtensionTests to expect 6 services and commented out OutboxInterceptor assertions. Removed OutboxInterceptorTests related to IIdentifiable storage in scoped resources.

--- src/IDFCR.Abstractions/Tests/IDFCR.Abstractions.Interceptors.DependencyInjection.Tests/ServiceCollectionExtensionTests.cs
+++ src/IDFCR.Abstractions/Tests/IDFCR.Abstractions.Interceptors.DependencyInjection.Tests/ServiceCollectionExtensionTests.cs
@@ -24,7 +24,7 @@ public class ServiceCollectionExtensionTests
     {
         services.AddInterceptors();
 
-        Assert.That(services, Has.Count.EqualTo(7));
+        Assert.That(services, Has.Count.EqualTo(6));
 
         var factoryService = services.FirstOrDefault(x => x.ImplementationType == typeof(DefaultEntityInterceptorFactory));
         Assert.That(factoryService, Is.Not.Null);
@@ -62,14 +62,14 @@ public class ServiceCollectionExtensionTests
             Assert.That(auditChangesService.Lifetime, Is.EqualTo(Microsoft.Extensions.DependencyInjection.ServiceLifetime.Transient));
         }
 
-        var outboxService = services.FirstOrDefault(x => x.ImplementationType == typeof(OutboxInterceptor));
-        Assert.That(outboxService, Is.Not.Null);
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(outboxService.ServiceType, Is.EqualTo(typeof(IEntityInterceptor)));
-            Assert.That(outboxService.ImplementationType, Is.EqualTo(typeof(OutboxInterceptor)));
-            Assert.That(outboxService.Lifetime, Is.EqualTo(Microsoft.Extensions.DependencyInjection.ServiceLifetime.Transient));
-        }
+        //var outboxService = services.FirstOrDefault(x => x.ImplementationType == typeof(OutboxInterceptor));
+        //Assert.That(outboxService, Is.Not.Null);
+        //using (Assert.EnterMultipleScope())
+        //{
+        //    Assert.That(outboxService.ServiceType, Is.EqualTo(typeof(IEntityInterceptor)));
+        //    Assert.That(outboxService.ImplementationType, Is.EqualTo(typeof(OutboxInterceptor)));
+        //    Assert.That(outboxService.Lifetime, Is.EqualTo(Microsoft.Extensions.DependencyInjection.ServiceLifetime.Transient));
+        //}
 
         var defaultScopedResource = services.FirstOrDefault(x => x.ImplementationType == typeof(DefaultScopedResources));
         Assert.That(defaultScopedResource, Is.Not.Null);
