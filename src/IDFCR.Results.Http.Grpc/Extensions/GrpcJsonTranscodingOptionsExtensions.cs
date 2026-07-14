90c8cc0807a7b5a2fc761b341c35bc2876cd7edb
Romesh Abeyawardena <romesh.a@outlook.com>
2026-07-14 22:49:34 +0100

    Add gRPC JSON transcoding support and extension
    Added Microsoft.AspNetCore.Grpc.JsonTranscoding package and project references. Introduced GrpcJsonTranscodingOptionsExtensions to configure TypeRegistry with UnitResult and custom descriptors for improved gRPC JSON serialization. Bumped project version to 3.1.1.3.

---
+++ src/IDFCR.Results.Http.Grpc/Extensions/GrpcJsonTranscodingOptionsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Google.Protobuf.Reflection;
+using Microsoft.AspNetCore.Grpc.JsonTranscoding;
+using V1Common = IDFCR.Abstractions.GRPC.Contracts.Common.V1;
+
+namespace IDFCR.Results.Http.Grpc.Extensions;
+
+/// <summary>
+/// Defines extension methods for configuring gRPC JSON transcoding options.
+/// </summary>
+public static class GrpcJsonTranscodingOptionsExtensions
+{
+    /// <summary>
+    /// Defines an extension method to configure the TypeRegistry for gRPC JSON transcoding options with the UnitResult type and any additional message descriptors.
+    /// </summary>
+    /// <param name="options">The gRPC JSON transcoding options to configure.</param>
+    /// <param name="descriptors">Additional message descriptors to include in the TypeRegistry.</param>
+    /// <returns>The configured gRPC JSON transcoding options.</returns>
+    public static GrpcJsonTranscodingOptions ConfigureUnitResultTypeRegistries(this GrpcJsonTranscodingOptions options, params MessageDescriptor[] descriptors)
+    {
+        options.TypeRegistry = TypeRegistry.FromMessages(
+           [V1Common.UnitResult.Descriptor, ..descriptors]);
+        return options;
+    }
+}
