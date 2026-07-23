e269e404b4b274d400248bd5b833b2983e5e59c8
Romesh Abeyawardena <Romesh.Abeyawardena@bellrock.co.uk>
2026-07-23 10:40:03 +0100

    Add abstractions for EF Core named delta operations
    Introduce INamed interface and NamedDeltaOptions<T> for configuring delta (add/remove) operations on many-to-many relationships by entity name. Add RelationshipDeltaResult to summarize changes, and DeltaExtensions with PerformDeltaAsync for managing relationship deltas. Include initial test scaffold in DeltaExtensionTests.cs.

---
+++ src/IDFCR.Abstractions/IDFCR.Abstractions.Metadata/INamed.cs
@@ -0,0 +1,12 @@
+﻿namespace IDFCR.Abstractions.Metadata;
+
+/// <summary>
+/// Represents an object that has a name property.
+/// </summary>
+public interface INamed
+{
+    /// <summary>
+    /// Gets the name of the entity.
+    /// </summary>
+    string Name { get; }
+}
---
+++ src/IDFCR.Persistence.EntityFramework/IDFCR.Persistence.EntityFrameworkCore.Tests/DeltaExtensionTests.cs
@@ -0,0 +1,59 @@
+﻿using IDFCR.Abstractions.Metadata;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace IDFCR.Persistence.EntityFrameworkCore.Tests;
+
+public class PackageDbContext : DbContext
+{
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<GroupTag> GroupsTag { get; set; }
+
+}
+
+public class Group : IIdentifiable<Guid>
+{
+    public ICollection<GroupTag> Tags { get; set; }
+    public Guid Id { get; set; }
+}
+
+public class Tag : INamed, IIdentifiable<Guid>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class GroupTag : IIdentifiable<Guid>
+{
+    public Guid Id { get; set; }
+    public Guid TagId { get; set; }
+    public Guid GroupId { get; set; }
+    public Group Group { get; set; }
+    public Tag Tag { get; set; }
+
+}
+
+
+[TestFixture]
+internal class DeltaExtensionTests
+{
+
+    [Test]
+    public void Test()
+    {
+        var tagOptions = new NamedDeltaOptions<Tag, GroupTag, Guid, Guid>
+        {
+            FilterEntitiesByNames = (query, names) => query.Where(x => names.Contains(x.Name)),
+            GetEntityName = tag => tag.Name,
+            GetEntityId = tag => tag.Id,
+            CreateNewEntity = name => new Tag { Name = name },
+
+            FilterExistingJoins = (query, pId, cIds) => query.Where(x => x.GroupId == pId && cIds.Contains(x.TagId)),
+            CreateNewJoin = (groupId, tag) => new GroupTag { GroupId = groupId, Tag = tag },
+
+            GetJoinChildId = groupTag => groupTag.TagId,
+            IsLocalJoin = (join, groupId, tag) => join.GroupId == groupId && ReferenceEquals(join.Tag, tag)
+        };
+    }
+}
